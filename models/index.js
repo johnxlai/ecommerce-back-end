954c9d5cc3bfdd6bac05b0029b5b8e0b038d422b
John Lai <lai.john.py@gmail.com>
2023-02-25 22:55:40 -0500

    Add delete route for tag

--- models/index.js
+++ models/index.js
@@ -7,25 +7,27 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   unique: false,
+  // // Define an alias for when data is retrieved
+  // as: 'product',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   unique: false,
+  // // Define an alias for when data is retrieved
+  // as: 'tag',
 });
 
 module.exports = {
